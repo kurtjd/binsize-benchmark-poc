a383d3afd7839080b2b95e8a8efad590203cc26b
github-action-benchmark <github@users.noreply.github.com>
2025-09-05 23:53:50 +0000

    add Rust Benchmark (customSmallerIsBetter) benchmark result for 6f1821eb18809969d5364f296fd8ac9aadc50f24

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1757115742417,
+  "lastUpdate": 1757116430322,
   "repoUrl": "https://github.com/kurtjd/binsize-benchmark-poc",
   "entries": {
     "Rust Benchmark": [
@@ -28,6 +28,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "Binary Size",
             "value": 342023,
+            "unit": "B"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "kdinelle@microsoft.com",
+            "name": "Kurtis Dinelle",
+            "username": "kurtjd"
+          },
+          "committer": {
+            "email": "kdinelle@microsoft.com",
+            "name": "Kurtis Dinelle",
+            "username": "kurtjd"
+          },
+          "distinct": true,
+          "id": "6f1821eb18809969d5364f296fd8ac9aadc50f24",
+          "message": "Add tokio to bloat size",
+          "timestamp": "2025-09-05T16:52:44-07:00",
+          "tree_id": "717dcb60ceb853f2985c739b9c27b0414fced8be",
+          "url": "https://github.com/kurtjd/binsize-benchmark-poc/commit/6f1821eb18809969d5364f296fd8ac9aadc50f24"
+        },
+        "date": 1757116429998,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Binary Size",
+            "value": 620136,
             "unit": "B"
           }
         ]
