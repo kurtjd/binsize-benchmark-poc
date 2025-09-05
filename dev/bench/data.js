47f5d2b79697e39ff0b6e21e99db2005741db234
github-action-benchmark <github@users.noreply.github.com>
2025-09-05 23:42:22 +0000

    add Rust Benchmark (customSmallerIsBetter) benchmark result for 0dccbec04f0d127edd66f82e341d84e4a8e3d856

---
+++ dev/bench/data.js
@@ -0,0 +1,37 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1757115742417,
+  "repoUrl": "https://github.com/kurtjd/binsize-benchmark-poc",
+  "entries": {
+    "Rust Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "kdinelle@microsoft.com",
+            "name": "Kurtis Dinelle",
+            "username": "kurtjd"
+          },
+          "committer": {
+            "email": "kdinelle@microsoft.com",
+            "name": "Kurtis Dinelle",
+            "username": "kurtjd"
+          },
+          "distinct": true,
+          "id": "0dccbec04f0d127edd66f82e341d84e4a8e3d856",
+          "message": "Add LLVM tools",
+          "timestamp": "2025-09-05T16:41:33-07:00",
+          "tree_id": "5f119b4359ff0d55b9bf77ad0bfe9c3c80429c37",
+          "url": "https://github.com/kurtjd/binsize-benchmark-poc/commit/0dccbec04f0d127edd66f82e341d84e4a8e3d856"
+        },
+        "date": 1757115741547,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Binary Size",
+            "value": 342023,
+            "unit": "B"
+          }
+        ]
+      }
+    ]
+  }
+}
